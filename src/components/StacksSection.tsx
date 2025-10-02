f86625e49450aa366d96a355014a8bb01f00187f
pedrohfc <pedrofreitas_ph@hotmail.com>
2025-10-02 00:25:36 -0300

    fix pls

--- src/components/StacksSection.tsx
+++ src/components/StacksSection.tsx
@@ -3,7 +3,7 @@ import nodejsIcon from "@/assets/icons/nodejs.svg";
 import postgresqlIcon from "@/assets/icons/postgresql.svg";
 import typescriptIcon from "@/assets/icons/typescript.svg";
 import restApiIcon from "@/assets/icons/rest-api.png";
-import jsIcon from "@/assets/icons/JavaScript-logo.png";
+// import jsIcon from "@/assets/icons/JavaScript-logo.png";
 import cypressIcon from "@/assets/icons/cypress-svgrepo-com.svg";
 import tailwindIcon from "@/assets/icons/tailwind_CSS_Logo.svg";
 import css3Icon from "@/assets/icons/css3_logo_and_wordmark.svg";
@@ -14,7 +14,7 @@ import pythonIcon from "@/assets/icons/python-logo-notext.svg";
 
 
 const stacks = [
-  { name: "Js", icon: jsIcon },
+  // { name: "Js", icon: jsIcon },
   { name: "Html", icon: htmlIcon },
   { name: "Css", icon: css3Icon },
   { name: "Git", icon: gitIcon },
